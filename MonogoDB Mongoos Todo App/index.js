889b59e88ce44cd63daf78860395ef99706e5bc9
MuahammadArsalan <muhammadarsalanforever@gmail.com>
2024-11-30 03:23:57 -0800

    CRUD Api's Completed

--- MonogoDB Mongoos Todo App/index.js
+++ MonogoDB Mongoos Todo App/index.js
@@ -2,9 +2,11 @@
 import express from "express"
 import connectDB from "./src/db/index.js"
 import dotenv from "dotenv"
+import cors from "cors"
 import todosRoutes from "./src/routes/todos.routes.js"
 dotenv.config()
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 
@@ -12,7 +14,7 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+})  
 
 // route
 app.use("/api/v1", todosRoutes);
