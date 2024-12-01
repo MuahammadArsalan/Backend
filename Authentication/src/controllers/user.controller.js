2590148855ed5e86e8a840b6c0dd882f42e6f894
MuahammadArsalan <muhammadarsalanforever@gmail.com>
2024-12-01 11:17:53 -0800

    auth

---
+++ Authentication/src/controllers/user.controller.js
@@ -0,0 +1,7 @@
+import Users from '../models/user.model.js'
+import  jwt  from 'jsonwebtoken'
+
+
+const generateAccessToken  = (user) =>{
+// return jwt.sign({email:user.email},) //iske aage ka JWT kiweb se url milega wo lagega
+}
--- Authentication/src/models/user.model.js
+++ Authentication/src/models/user.model.js
@@ -1,3 +1,21 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Types } from "mongoose";
 
 
+const Schema = mongoose.Schema
+
+const userSchema = new Schema(
+    {
+        email:{
+            type:String,
+            required:[true,'Email is required'],
+            unique:true
+        
+        },
+        password:{
+            type:String,
+            required:[true,'Password is required']
+        }
+    }
+)
+
+export default mongoose.model('Users',userSchema)
